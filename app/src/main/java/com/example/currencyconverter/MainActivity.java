d3de1f719600f0e45add32f8a7c049c40413665b
OzodTagoev <ozod.tagoev@gmail.com>
2021-04-18 14:45:03 +0500

    Converting currency

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,8 +3,21 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void currencyConverter(View view) {
+
+        Log.i("Info", "Button pressed!");
+
+        TextView currencyEditText = findViewById(R.id.currencyEditText);
+        Double currencyAmount = Double.parseDouble(currencyEditText.getText().toString());
+        Toast.makeText(this, "£" + currencyAmount + " is $" + 1.38 * currencyAmount, Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
